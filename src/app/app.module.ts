387602040b783fb64ccf3d63fde12d51a29aec73
16520285 <kudophuongduy@gmail.com>
2020-09-16 23:06:00 +0700

    Implement post UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppsHomeViewComponent } from './view/home/apps-home-view-component.view';
-import { AppsPostComponent } from './component/posts/index'
+import { AppsPostComponent } from './component/posts/index';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,8 +18,16 @@ import { AppsPostComponent } from './component/posts/index'
     AppsPostComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,    
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+
+    BrowserAnimationsModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/posts/post.component.ts
+++ src/app/component/posts/post.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'apps-post-component',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'apps-post-component',
+	templateUrl: './post.component.html',
+	styleUrls: ['./post.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class AppsPostComponent implements OnInit {
 
-  constructor() { }
+	photosList = [
+		{ src: `https://i.imgur.com/nXgEtSy.jpg` },
+		{ src: `https://i.imgur.com/FcxINEt.jpg` },
+		{ src: `https://i.imgur.com/bJiRyI1.jpg` },
+		{ src: `https://i.imgur.com/eqhxhmi.jpg` },
 
-  ngOnInit() {
-  }
+	]
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
